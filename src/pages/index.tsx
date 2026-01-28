e3c17e5857ebdd57e7bb1120ec6f83bc4b490036
SampathHM <t3hesa01@students.oamk.fi>
2026-01-28 21:31:56 +0200

    Add index.tsx file

---
+++ src/pages/index.tsx
@@ -0,0 +1,15 @@
+import CourseContent from "@/components/CourseContent";
+
+const Index = () => {
+  return (
+    <div className="flex h-screen bg-background">
+      {/* Course Content - Left Side */}
+      <CourseContent />
+
+      {/* Chat Sidebar - Right Side (will add later) */}
+      {/* <ChatSidebar /> */}
+    </div>
+  );
+};
+
+export default Index;
